52b5119bf485de5ae85856a6105b1cc46507ab50
azubuike okom <azubuikeokom@gmail.com>
2022-08-19 22:06:34 +0100

    added content type to bucket object

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_s3_object" "object" {
   key    = each.value
   source = "frontend-files/${each.value}"
   etag = filemd5("frontend-files/${each.value}")
-  content = "html/text"
+  content_content_type =  "text/html"
 }
 resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
   bucket = aws_s3_bucket.main.id
